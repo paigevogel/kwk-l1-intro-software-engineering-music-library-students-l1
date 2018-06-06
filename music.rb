06195860d9207eb9706292954797afbe32966d2e
paigevogel <paigevogel36@gmail.com>
2018-06-06 23:07:36 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -1,9 +1,23 @@
+def list_artists(library)
+  library.each_key { |artist|
+    puts artist 
+  }
+end
 
-#1. Define a method named `list_artists`. This method should use the `each_key` method to iterate over the hash and return all the artists (the keys)
-# This method should accept a hash as an argument.
+  artists = {
+    "The Beatles" => "Yellow Submarine",
+    "Led Zeppelin" => "Good Times Bad Times",
+    "The Rolling Stones" => "Paint it Black"
+  }
+  
+#list_artists(artists)
 
-#2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
-# This method should accept a hash as an argument.
+def list_songs(library)
+  library.each_value { |artist| 
+    puts artist
+  }
+end
+list_songs(songs)
 
 #3. Define a method `delete_artist`. This method should take delete the key-value pair from the hash based on the artist and returned the updated hash.
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol. 
